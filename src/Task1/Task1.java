8c69bbf44bf3d19cef5ff286219b07b4af1cdbc7
Кристина Алексеенко <kristina.aleksejenko@ivkhk.ee>
2021-10-05 22:20:46 +0300

    first

---
+++ src/Task1/Task1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Создать проект с названием JavaHello. Программа должна выводить в консоль слова “Java Hello!”. 
+//Создать локальный репозиторий Git. Создать удаленный репозиторий и связать с ним локальный. 
+//Загрузить код проекта на удаленный репозиторий Git.
+package Task1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class Task1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("JAVA Hello!");// print
+    }
+    
+}
